4a216f65b78d1bb19a561d39e04af1c80d5ce0bb
Misty <gyc990326@gmail.com>
2021-08-06 03:11:05 +0800

    Move ssh into private folder

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ ln -s $basedir/.alacritty.yml ~/.alacritty.yml
 ln -s $basedir/.vimrc ~/.vimrc
 ln -s $basedir/.tmux.conf.local ~/.tmux.conf.local
 ln -s $basedir/.zshrc ~/.zshrc
-ln -s $basedir/.ssh/config ~/.ssh/config
+
+ln -s $basedir/_private/.ssh/config ~/.ssh/config
