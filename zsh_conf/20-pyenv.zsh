dcce4148b3f859ff1f3bddc80dff191b844cfe77
Misty <gyc990326@gmail.com>
2021-11-25 15:10:32 +0800

    Support changing pyenv root

--- zsh_conf/20-pyenv.zsh
+++ zsh_conf/20-pyenv.zsh
@@ -1,7 +1,7 @@
 #########################################################################
 #
 # pyenv configs
-export PYENV_ROOT="/opt/pyenv"
+export PYENV_ROOT="${PYENV_ROOT:-/opt/pyenv}"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init --path)"
