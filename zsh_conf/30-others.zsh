b478f83f8dd0a18245d6d23d5c5b71cd0ecfd8ff
Misty <gyc990326@gmail.com>
2022-02-21 19:17:08 +0800

    Better newenv command & newsystemd command for simple dev docker

--- zsh_conf/30-others.zsh
+++ zsh_conf/30-others.zsh
@@ -11,7 +11,30 @@ function docker_update_images {
     docker image prune
 }
 
-alias newenv='docker run --rm -v ${PWD}:/workdir -w /workdir -it nyamisty/dev'
+function last_docker {
+    docker ps -q -l
+}
+
+function run_simpledev {
+    docker_param=$1
+    command_line=$2
+    sudo touch ~/.newenv_zsh_history ~/.newenv_zsh_history.new
+    eval 'docker run --rm \
+        -v ~/.newenv_zsh_history:/root/.zsh_history \
+        -v ${PWD}:/workdir -w /workdir \
+        --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro' \
+        $docker_param \
+        '-it nyamisty/dev' \
+        $command_line
+}
+
+#alias newenv='touch ~/.newenv_zsh_history ~/.newenv_zsh_history.new; docker run --rm -v ${PWD}:/workdir -v ~/.newenv_zsh_history:/root/.zsh_history -w /workdir -it nyamisty/dev'
+alias newenv='run_simpledev'
+function newsystemd {
+    container=$(run_simpledev "-d $1" systemd)
+    docker exec -it "$container" zsh
+    docker rm -fv $container
+}
 
 alias vimzshrc="vim /mnt/d/Envs/LinuxEnvs/.zshrc; exec $SHELL"
 alias vimrc="vim /mnt/d/Envs/LinuxEnvs/"
