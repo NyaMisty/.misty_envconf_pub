6d7e261e8733fef3c7b01f9e486d877f3a6eaa9f
Misty <gyc990326@gmail.com>
2022-06-16 08:08:44 +0800

    Remove Windows paths during ZSH init

--- zsh_conf/00--path.zsh
+++ zsh_conf/00--path.zsh
@@ -1,1 +1,27 @@
-export PATH=$HOME/.bin:/usr/local/bin:/usr/sbin:/sbin:$PATH
+#!/usr/bin/zsh
+# Clean dup path first, 
+# then clean /mnt path entries before oh-my-zsh init, 
+# finally restore them afterwards
+
+# In ZSH, $path and $PATH are tied array
+
+typeset -U path
+
+export ORIPATH=$PATH
+
+# Clean Windows PATH
+#IFS=: read -r -a patharr <<<"$PATH"
+#for index in "${!patharr[@]}" ; do 
+#    [[ ${patharr[$index]} =~ ^/mnt/.* ]] && unset -v 'patharr[$index]'
+#done
+#patharr=("${patharr[@]}")
+
+#patharr=("${(@s/:/)PATH}")
+
+path=(${path:#/mnt*})
+
+#export PATH=$( printf '%s:' "${patharr[@]}" )
+
+export WSL_REQUIRED_PATH="/mnt/c/Program Files/Git/cmd"
+export PATH=$HOME/.bin:/usr/local/bin:/usr/sbin:/sbin:$WSL_REQUIRED_PATH:$PATH
+typeset -U path
---
+++ zsh_conf/13-pathrestore.zsh
@@ -0,0 +1,3 @@
+# restore Windows PATH after OMZ initialize
+export PATH=$PATH:$ORIPATH
+typeset -U path
