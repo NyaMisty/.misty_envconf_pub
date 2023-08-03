8e8496959205e6e4096f2dda413ac3ff71d35e40
Misty <gyc990326@gmail.com>
2023-08-03 14:26:52 +0800

    Change to async push & check for user interrupt

--- zsh_conf/50-push.zsh
+++ zsh_conf/50-push.zsh
@@ -28,30 +28,35 @@ prompt_preexec() {
 prompt_precmd() {
   if (( prompt_prexec_realtime )); then
     local -rF elapsed_realtime=$(( EPOCHREALTIME - prompt_prexec_realtime ))
-    
-    if (( elapsed_realtime > 20 )) && \
-      ! [[ "$prompt_preexec_cmd" =~ ^(htop|top|btop|watch).* ]] \
-      ; then
-      local -rF s=$(( elapsed_realtime%60 ))
-      local -ri elapsed_s=${elapsed_realtime}
-      local -ri m=$(( (elapsed_s/60)%60 ))
-      local -ri h=$(( elapsed_s/3600 ))
-
-      if (( h > 0 )); then
-        printf -v prompt_elapsed_time '%ih%im' ${h} ${m}
-      elif (( m > 0 )); then
-        printf -v prompt_elapsed_time '%im%is' ${m} ${s}
-      elif (( s >= 10 )); then
-        printf -v prompt_elapsed_time '%.2fs' ${s} # 12.34s
-      elif (( s >= 1 )); then
-        printf -v prompt_elapsed_time '%.3fs' ${s} # 1.234s
-      else
-        printf -v prompt_elapsed_time '%ims' $(( s*1000 ))
-      fi
-
-      pushex "cmd finish in ${prompt_elapsed_time} on $(uname -n)" "cmd: ${prompt_preexec_cmd}" "$MISTYPUSH_CHAN"
-    fi
     unset prompt_prexec_realtime
+
+    if [[ $? -eq 130 ]]; then
+      return
+    fi
+    if (( elapsed_realtime < 40 )); then
+      return
+    fi
+    if [[ "$prompt_preexec_cmd" =~ ^(htop|top|btop|watch|vim).* ]]; then
+      return
+    fi
+    local -rF s=$(( elapsed_realtime%60 ))
+    local -ri elapsed_s=${elapsed_realtime}
+    local -ri m=$(( (elapsed_s/60)%60 ))
+    local -ri h=$(( elapsed_s/3600 ))
+
+    if (( h > 0 )); then
+      printf -v prompt_elapsed_time '%ih%im' ${h} ${m}
+    elif (( m > 0 )); then
+      printf -v prompt_elapsed_time '%im%is' ${m} ${s}
+    elif (( s >= 10 )); then
+      printf -v prompt_elapsed_time '%.2fs' ${s} # 12.34s
+    elif (( s >= 1 )); then
+      printf -v prompt_elapsed_time '%.3fs' ${s} # 1.234s
+    else
+      printf -v prompt_elapsed_time '%ims' $(( s*1000 ))
+    fi
+
+    (pushex "cmd finish in ${prompt_elapsed_time} on $(uname -n)" "cmd: ${prompt_preexec_cmd}" "$MISTYPUSH_CHAN" &)
   else
     # Clear previous result when hitting ENTER with no command to execute
     unset prompt_elapsed_time
