bbe9cb89387a14134c930cd8d5e7ab466dd31734
Sakuragawa Misty <gyc990326@gmail.com>
2025-06-29 11:16:01 +0800

    Add gvmdocker command

--- zsh_conf/50-gvm.zsh
+++ zsh_conf/50-gvm.zsh
@@ -1,2 +1,4 @@
 export GVM_DIR="$HOME/.gvm"
 [[ -s "$GVM_DIR/scripts/gvm" ]] && source "$GVM_DIR/scripts/gvm"
+
+alias gvmdocker="dockertest -v $""HOME/.gvm:/root/.gvm -v /mnt:/mnt -w $""PWD nyamisty/dev-go zsh -c '. ~/.zshrc; git config --global --add safe.directory "'"'"*"'"'"; gvm use '$""gvm_go_name'; set -x; $""@ ' '' "
