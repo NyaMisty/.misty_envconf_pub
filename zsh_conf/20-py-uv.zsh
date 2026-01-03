11ecf9f295c482d5fe84792d598a24e2187d3b36
Sakuragawa Misty <gyc990326@gmail.com>
2026-01-04 00:21:00 +0800

    Add python uv support

---
+++ zsh_conf/20-py-uv.zsh
@@ -0,0 +1,8 @@
+export UV_PROJECT_ENVIRONMENT=.venv_wsl
+
+export UV_DIR=${UV_DIR:-/opt/uv}
+export UV_TOOL_DIR=$UV_DIR/tools
+export UV_TOOL_BIN_DIR=$UV_DIR/tools
+export UV_PYTHON_INSTALL_DIR=$UV_DIR/python
+export UV_PYTHON_BIN_DIR=$UV_DIR/bin
+export PATH=$PATH:$UV_PYTHON_BIN_DIR
